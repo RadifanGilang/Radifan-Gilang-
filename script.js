44a1f054675f49457eb09ba2bb539f17405c5fd8
RadifanGilang <radifangilang123@gmail.com>
2024-11-18 17:44:15 +0700

    UTS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const slide = document.getElementById("slide");
-conts mainContent = document.getElementById("main-content");
+const mainContent = document.getElementById("main-content");
 const toggleBtn = document.getElementById("toggle");
 
 toggleBtn.onclick = function() {
